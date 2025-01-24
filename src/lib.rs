b79314d849114b6d04018d8d518f1691d4469839
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-24 04:10:59 +0100

    Refactor solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
 pub fn is_leap_year(year: u64) -> bool {
     if year % 100 == 0 {
-        if year % 400 == 0 {
-            true
-        } else {
-            false
-        }
-    } else if year % 4 == 0 {
-        true
+        year % 400 == 0
     } else {
-        false
+        year % 4 == 0
     }
 }
