c952389b8d65a3aaee3d3205a1b1d9860d61dc87
Doken Edgar <dokenedgarjr@gmail.com>
2025-01-24 04:00:33 +0100

    Add code to determine if a year is a leap year

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_leap_year(year: u64) -> bool {
-    todo!("true if {year} is a leap year")
+    if year % 100 == 0 {
+        if year % 400 == 0 {
+            true
+        } else {
+            false
+        }
+    } else if year % 4 == 0 {
+        true
+    } else {
+        false
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,5 @@
-fn main() {}
+fn main() {
+    let year = 2000;
+    let is_leap = leap::is_leap_year(year);
+    println!("{} is a leap year? {}", year, is_leap);
+}
